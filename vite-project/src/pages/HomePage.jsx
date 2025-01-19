b8e1b0c352b57783cd9981b54415c0af9b1a6fbc
Wafeeqa Chowdhury <wafeeqarashid@gmail.com>
2025-01-18 19:01:34 -0500

    frontend page started

--- vite-project/src/pages/HomePage.jsx
+++ vite-project/src/pages/HomePage.jsx
@@ -1,5 +1,26 @@
 const HomePage = () => {
-    return <h1 className="header">Trojo</h1>;
-  };
-  
-  export default HomePage;
+  return (
+    <div className="homepage-container">
+      {/* Flex container for the two main sections */}
+      <div className="flex-container">
+        {/* Safety Section */}
+        <div className="safety-section">
+          <h1 className="safety-header">Safety made easy</h1>
+          <p className="safety-description">
+            This is a short description of the product and/or a phrase that makes people want to click!
+          </p>
+          <button className="get-started-button">Get started</button>
+        </div>
+
+        {/* Trojo Section */}
+        <div className="trojo-section">
+          <h1 className="trojo-header">
+            Tro<span className="long-j">j</span>o
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
